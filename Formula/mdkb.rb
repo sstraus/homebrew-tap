1d5c4687fda799563aeb8cb0aef3189457af4027
Stefano Straus <stefano@straus.it>
2026-04-30 10:35:34 +0200

    fix: remove double version interpolation in mdkb formula URLs

--- Formula/mdkb.rb
+++ Formula/mdkb.rb
@@ -6,20 +6,20 @@ class Mdkb < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sstraus/mdkb/releases/download/v#{version}#{version}/mdkb-macos-arm64"
+      url "https://github.com/sstraus/mdkb/releases/download/v#{version}/mdkb-macos-arm64"
       sha256 "78a22fa89b4fe1952275c5a74ec543c576ea538b32c9474360602d232b4b0349"
     else
-      url "https://github.com/sstraus/mdkb/releases/download/v#{version}#{version}/mdkb-macos-x64"
+      url "https://github.com/sstraus/mdkb/releases/download/v#{version}/mdkb-macos-x64"
       sha256 "50953e0b6515e9bd496756357d8d698e9cc1218b78c891df63dc7e5c0a87af84"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/sstraus/mdkb/releases/download/v#{version}#{version}/mdkb-linux-arm64"
+      url "https://github.com/sstraus/mdkb/releases/download/v#{version}/mdkb-linux-arm64"
       sha256 "a2882a767a9f945d78167f38ecab63f7a0f91a83e90b6bfc81eede6a955764a3"
     else
-      url "https://github.com/sstraus/mdkb/releases/download/v#{version}#{version}/mdkb-linux-x64"
+      url "https://github.com/sstraus/mdkb/releases/download/v#{version}/mdkb-linux-x64"
       sha256 "b2190f5ea91217b5e83388152a4bb3646fa8d18f7a97e71660c5fd806a2e5228"
     end
   end
